811e6c96a163e0a9b1898786e88bdbb75b7649aa
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-09-03 06:39:22 +0300

    add basic files with ref

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,33 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    home
+    <p ref="p">My name is {{ name.toUpperCase() }} and my age is {{ age }}</p>
+    <button @click="handleClick">click me</button>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue';
+
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  setup() {
+    console.log(this);
+
+    const p = ref('hello');
+
+    let name = 'mario';
+    let age = 30;
+
+    const handleClick = () => {
+      console.log(p, p.value);
+      p.value.classList.add('test');
+      p.value.textContent = 'hello, ninjas';
+    };
+
+    return { name, age, handleClick, p };
+  },
+};
 </script>
